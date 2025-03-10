2dbfc0b301ab7fdd67c33336067e90d95265c62d
anshul312 <choudharyanshul962@gmail.com>
2025-03-10 23:59:59 +0530

    added moving camera,more tiles , world map

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -3,7 +3,7 @@ package entity;
 import java.awt.image.BufferedImage;
 
 public class Entity {
-    public double x,y;
+    public double worldX,worldY;
     public int speed;
 
     public BufferedImage up1,up2,down1,down2,left1,right1,left2,right2;
