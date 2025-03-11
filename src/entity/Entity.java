82ca5b8cdd55c61ef88f93596202bae248c9476c
anshul312 <choudharyanshul962@gmail.com>
2025-03-11 15:10:00 +0530

    Revert "Blue_Boy Adventure"

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,15 +1,10 @@
 package entity;
 
-
 import java.awt.*;
-
 import java.awt.image.BufferedImage;
 
 public class Entity {
-
-
     public double worldX,worldY;
-
     public int speed;
 
     public BufferedImage up1,up2,down1,down2,left1,right1,left2,right2;
@@ -18,7 +13,6 @@ public class Entity {
 
     public int spriteCounter=0;
     public int spriteNum=1;
-
     public Rectangle collisionArea;
     public boolean collisionOn=false;
 
--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -1,16 +1,13 @@
 package tile;
 
-
 import main.GamePanel;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
 
 public class TileManager {
     GamePanel gp;
